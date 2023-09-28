483d4c5e9a98e7743e72c869b4d399b4813a9e79
tarun sirpi <tarunsirpi@gmail.com>
2023-09-28 22:42:53 +0530

    updated logger, exception

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -22,16 +22,23 @@ class DataIngestion():
     self.ingestion_config = DataIngestionConfig()
 
   def initiate_data_ingestion(self):
+    logging.info("Starting data ingestion")
     try:
       df = pd.read_csv(os.path.join("notebooks/data/cleandata.csv"))
+      logging.info("Raw date reading completed")
 
       os.makedir(os.path.dirname(self.injestion_config.raw_data_path), exist_ok = True)
 
       df.to_csv(self.injestion_config.raw_data_path)
 
       train_set, test_set = train_test_split(df,test_size=0.25, random_state=42)
+      logging.info("train and test split complete")
+
       train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
       test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+      logging.info("data ingestion complete")
+
+      return(self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
 
     except Exception as ex:
       raise Exception
---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+from datetime import datetime
+import os
+
+LOG_FILE = f"{datetime.now().strftime('%b_%d_%Y_%H:%M:%S')}.log"
+
+log_path = os.path.join(os.getcwd(),"logs", LOG_FILE)
+
+print(LOG_FILE )
+print(log_path)
+os.makedirs(os.path.join(os.getcwd(),"logs"), exist_ok=True)
+
+
+LOG_FILE_PATH=os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(filename= LOG_FILE_PATH , level = logging.INFO, format="[%(asctime)s]%(lineno)d %(name)s - %(levelname)s %(message)s")
+
+print(LOG_FILE_PATH )
